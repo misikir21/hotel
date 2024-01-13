1af8eec5d7d8d0a2934eced12bb36ab449af26a4
misikir21 <misikirkaka@gmail.com>
2024-01-13 10:15:45 +0300

    fix all errors

--- src/app/component/Header/Header.tsx
+++ src/app/component/Header/Header.tsx
@@ -6,8 +6,9 @@ function Header() {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-[#f27405]" />
-        HotelPana
+        <Link href="/" className="font-black text-tertiary-dark">
+          HotelPana
+        </Link>
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
             <Link href="/auth">
